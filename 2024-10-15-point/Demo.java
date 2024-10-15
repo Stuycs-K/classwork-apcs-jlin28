c183475fb8e5279d762147f2ad8a1fe2c885b381
Joyce Lin <jlin60@stuy.edu>
2024-10-15 17:58:46 -0400

    Added code that allows for the three sides of an equilateral triangle to be printed

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -11,6 +11,7 @@ public class Demo{
   }
 
   public static void main(String[]args){
+    double midpoint;
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
@@ -19,11 +20,23 @@ public class Demo{
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
+
     //start of equilateral triangle
     Point a = new Point(0,0);
     Point b = new Point(0,1);
-    int triangley = Math.sqrt()
-    Point c = new Point(Math.sqrt(3/4), 3/4);
-    System.print
+    Point c;
+    if (a.getX() != b.getX()) {
+      midpoint = (a.getX() + b.getX())/2.0;
+      c = new Point(midpoint, midpoint * Math.sqrt(3));
+    }
+    else {
+      midpoint = (a.getY() + b.getY())/2.0;
+      c = new Point(midpoint * Math.sqrt(3), midpoint);
+    }
+    System.out.println("\nThe distances between each point is: ");
+    System.out.println(distance(a,b) + " a -- b");
+    System.out.println(Point.distance(b,c) + " b -- c");
+    System.out.println(a.distanceTo(c) + " a -- c");
+    System.out.println(c + " are the coordinates of point C");
   }
 }
