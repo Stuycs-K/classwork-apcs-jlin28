fd0d7a4e8a7e080cc75d921971bab7daa2d44042
Joyce Lin <jlin60@stuy.edu>
2024-11-02 14:08:14 -0400

    coded countTrianglesB as well ass added a inputB.txt

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -33,7 +33,39 @@ public class TriangleTester {
     return count;
   }
 
+  public static int countTrianglesB(String filename) {
+    int count = 0;
+    try {
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      int[][] nums = new int[3][3];
+      for (int x = 0, i = 0; input.hasNextInt() || x == 3 || i == 3;) {
+        if (i == 3) {
+          for (int row = 0; row < 3; row++) {
+            if (isTriangle(nums[row][0], nums[row][1], nums[row][2])) {
+              count++;
+            }
+          }
+          i = 0;
+        }
+        else if (x == 3) {
+          x = 0;
+          i++;
+        }
+        else {
+          nums[x][i] = input.nextInt();
+          x++;
+        }
+      }
+      input.close();
+    }catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+    return count;
+  }
+
   public static void main(String[] args) {
     System.out.println(countTrianglesA("inputA.txt"));
+    System.out.println(countTrianglesB("inputTri.txt"));
   }
 }
