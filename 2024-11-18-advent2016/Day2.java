985bffbb7baec8ef8b5d8224f2f6d8251fb7f850
Joyce Lin <jlin60@stuy.edu>
2024-11-19 22:30:06 -0500

    Finished Day 2 Part 1

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -16,19 +16,46 @@ public class Day2 {
     return lines;
   }
 
-  public static int solve(ArrayList<String> lines) {
-    String[][] keys = new String { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
-    String pass = "";
-    int posx, posy;
-    for (int x = 0; x < lines.size; x++) {
-      String line = lines.get(x);
-      for (int i = 0; i < line.length(); i++) {
-        if ()
-      }
+  public static int pos(char dir) {
+    if (dir == 'U') {
+      return -3;
     }
+    else if (dir == 'D') {
+      return 3;
+    }
+    else if (dir == 'R') {
+      return 1;
+    }
+    return -1;
+  }
+
+  public static int solve(ArrayList<String> lines) {
+    String pass = "";
+    int lastpos = 5;
+    for (int x = 0; x < lines.size(); x++) {
+      for (int i = 0; i < lines.get(x).length(); i++) {
+        char temp = lines.get(x).charAt(i);
+        if (temp == 'U' || temp == 'D') {
+          if (lastpos + pos(temp) > 0 && lastpos + pos(temp) < 10) {
+            lastpos = lastpos + pos(temp);
+          }
+        }
+        else if ((lastpos == 1 || lastpos == 4 || lastpos == 7) && temp == 'R') {
+          lastpos = lastpos + pos(temp);
+        }
+        else if ((lastpos == 3 || lastpos == 6 || lastpos == 9) && temp == 'L') {
+          lastpos = lastpos + pos(temp);
+        }
+        else if ((lastpos == 2 || lastpos == 5 || lastpos == 8)){
+          lastpos = lastpos + pos(temp);
+        }
+      }
+      pass = pass + lastpos;
+    }
+    return Integer.parseInt(pass);
   }
 
   public static void main(String[] args) {
-    System.out.println(parse("input2.txt"));
+    System.out.println(solve(parse("input2.txt")));
   }
 }
