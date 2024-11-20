ea93b1f11953207b6a9be156aa2dce11a6bbe539
Joyce Lin <jlin60@stuy.edu>
2024-11-19 23:49:11 -0500

    Trying part 2 of Day2

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -16,6 +16,19 @@ public class Day2 {
     return lines;
   }
 
+  public static int pos2(char dir) {
+    if (dir == 'U') {
+      return -4;
+    }
+    else if (dir == 'D') {
+      return 4;
+    }
+    else if (dir == 'R') {
+      return 1;
+    }
+    return -1;
+  }
+
   public static int pos(char dir) {
     if (dir == 'U') {
       return -3;
@@ -36,7 +49,13 @@ public class Day2 {
       for (int i = 0; i < lines.get(x).length(); i++) {
         char temp = lines.get(x).charAt(i);
         if (temp == 'U' || temp == 'D') {
-          if (lastpos + pos(temp) > 0 && lastpos + pos(temp) < 10) {
+          if (temp == 'D' && (lastpos == 1 || lastpos == 11)) {
+            lastpos = lastpos + pos(temp) - 2;
+          }
+          else if (temp == 'U' && (lastpos == 13 || lastpos == 3)) {
+            lastpos = lastpos + pos(temp) + 2;
+          }
+          else if (lastpos + pos(temp) > 0 && lastpos + pos(temp) < 10) {
             lastpos = lastpos + pos(temp);
           }
         }
@@ -55,7 +74,33 @@ public class Day2 {
     return Integer.parseInt(pass);
   }
 
+  public static String solve2(ArrayList<String> lines) {
+    String[] keys = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D"};
+    String pass = "";
+    int lastpos = 5;
+    for (int x = 0; x < lines.size(); x++) {
+      for (int i = 0; i < lines.get(x).length(); i++) {
+        char temp = lines.get(x).charAt(i);
+        if (temp == 'U' || temp == 'D') {
+          if (lastpos + pos(temp) > 0 && lastpos + pos(temp) < 14 && lastpos != 5 && lastpos != 9) {
+            lastpos = lastpos + pos(temp);
+          }
+        }
+        else if (lastpos != 1 && lastpos != 2 && lastpos != 5 && lastpos != 10 && lastpos != 13 && temp == 'L') {
+          lastpos = lastpos + pos(temp);
+        }
+        else if (lastpos != 1 && lastpos != 4 && lastpos != 9 && lastpos != 12 && lastpos != 13 && temp == 'R') {
+          lastpos = lastpos + pos(temp);
+        }
+      }
+      pass = pass + keys[lastpos-1];
+    }
+    return pass;
+  }
+
   public static void main(String[] args) {
     System.out.println(solve(parse("input2.txt")));
+    System.out.println(solve2(parse("input2.txt")));
   }
+
 }
