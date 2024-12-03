26ad15d89715bff81d92485f52986b266a277bc8
Joyce Lin <jlin60@stuy.edu>
2024-12-03 10:12:56 -0600

    Pasted in go, color and strings with small edits to variable names + created main

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,17 @@
+public class ColorDemo {
+  public static final String clear =  "\u001b[2J";
+  public static final String hide =  "\u001b[?25l";
+  public static final String show =  "\u001b[?25h";
+
+  public static void go(int x,int y){
+    System.out.print("\u001b[" + y + ";" + x + "f");
+  }
+
+  public static void color(int fg, int bg, int modifier) {
+    System.out.println("\u001b[" + fg + ";" + bg + ";" + modifier + "m");
+  }
+  
+  public static void main(String[] args) {
+    System.out.print()
+  }
+}
