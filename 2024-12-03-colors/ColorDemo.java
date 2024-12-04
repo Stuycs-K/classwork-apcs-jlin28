643dea3b579e148209124168cc4a87ac6a27bcb3
Joyce Lin <jlin60@stuy.edu>
2024-12-03 23:04:07 -0500

    Created a little drawing and added a delay for a second drawing

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -11,10 +11,42 @@ public class ColorDemo {
     System.out.println("\u001b[" + fg + ";" + bg + ";" + modifier + "m");
   }
 
+  public static void sleep(int milli){
+      try{
+              Thread.sleep(milli);
+      }catch(Exception e){
+      }
+  }
+
   public static void main(String[] args) {
-    go(20, 20);
-    color()
+    color(36, 49, 1);
+    System.out.println("     ______________                 ______________     ");
+    System.out.println("    /              \\               /              \\  ");
+    System.out.println("   |                |             |                |");
+    System.out.println("   |      \u001b[32;42m[][]\u001b[36;49m      |-------------|      \u001b[32;42m[][]\u001b[36;49m      |");
+    System.out.println("   |                |             |                |");
+    System.out.println("    \\              /               \\              /  ");
+    System.out.println("     --------------                 --------------     ");
+    System.out.println();
+    color(37, 49, 1);
+    System.out.println("               \u001b[47m-------------\u001b[93;103m---\u001b[37;47m---------\u001b[49m");
+    System.out.println("                 \u001b[31;41m---------------------\u001b[49m");
+    System.out.println("                   \u001b[31;41m-----------------\u001b[49m");
+    System.out.println("                     \u001b[31;41m-----\u001b[91;101m---\u001b[31;41m-----\u001b[49m");
+    System.out.print("\u001b[H");
+    color(31, 49, 1);
+    sleep(1000);
+    System.out.println("         \u001b[41m.... \u001b[49m      \u001b[41m.....\u001b[49m");
+    System.out.println("       \u001b[41m....................\u001b[49m");
+    System.out.println("       \u001b[41m....................\u001b[49m");
+    System.out.println("         \u001b[41m................\u001b[49m");
+    System.out.println("           \u001b[41m............\u001b[49m");
+    System.out.println("             \u001b[41m........\u001b[49m");
+    System.out.println("               \u001b[41m....\u001b[49m");
+
     // resetting terminal to defaults
     System.out.print("\u001b[0m");
+    sleep(2500);
+    System.out.print("\u001b[2J");
   }
 }
