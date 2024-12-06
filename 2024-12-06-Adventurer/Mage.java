dd899d1c07f37a9605727668d919c897b6b979fd
Joyce Lin <jlin60@stuy.edu>
2024-12-06 09:49:33 -0600

    Added private variables for special attribute and created the corresponding methods

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -1,10 +1,35 @@
 public class Mage extends Adventurer {
+  private int maxMP;
+  private int mp;
+
   public Mage (String name) {
     super(name);
   }
 
-  public Mage (String name, int hp) {
+  public Mage (String name, int hp, int mp) {
     super(name, hp);
+    this.mp = mp;
+    this.maxMP = mp;
   }
 
+  public String getSpecialName() {
+    return "mp";
+  }
+
+  public int getSpecial() {
+    return mp;
+  }
+
+  public void setSpecial(int n) {
+    if (n > 0 && n < maxMP) {
+      this.mp = n;
+    }
+    else {
+      throw new IllegalArgumentException("MP cannot be negative or above max MP of" + maxMP);
+    }
+  }
+
+  public int getSpecialMax() {
+    return maxMP;
+  }
 }
