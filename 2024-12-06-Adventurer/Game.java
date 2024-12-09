dbfc8c3f99a2baf6431c3120353377824f08c335
Joyce Lin <jlin60@stuy.edu>
2024-12-09 10:00:04 -0600

    Coded up to asking for action input

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -12,6 +12,15 @@ public class Game {
     String userName = userInput.nextLine();
     //Do something with the input
     Adventurer p1 = new Mage(userName, 100, 100);
+    Adventurer p2 = new CodeWarrior();
     System.out.println("New character created: " + p1 + ", " + p1.getHP() + "/" + p1.getmaxHP() + " HP, " + p1.getSpecial() + "/" + p1.getSpecialMax() + " MP");
+    System.out.println("An opponent CodeWarrior has appeared!");
+    System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+    String action = userInput.nextLine();
+    while (!(action.equals("quit"))) {
+      if (action.equals("a")) {
+        p1.attack(p2);
+      }
+    }
   }
 }
