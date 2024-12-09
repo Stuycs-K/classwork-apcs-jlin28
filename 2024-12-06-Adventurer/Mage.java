fe9c06402ad6858a16159d7a818b1db2fa0ddfd9
Joyce Lin <jlin60@stuy.edu>
2024-12-08 19:27:18 -0500

    Coded attack and healing/buffs methods as well as added accessor methods + set method for MP (special resource)

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -32,4 +32,64 @@ public class Mage extends Adventurer {
   public int getSpecialMax() {
     return maxMP;
   }
+
+  public String attack(Adventurer other) {
+    if (other.getHP() - 10 > 0) {
+      other.applyDamage(10);
+    }
+    else {
+      other.setHP(0);
+    }
+    return "10 damage dealt with Basic Attack!";
+  }
+
+  public String support(Adventurer other) {
+    if (other.getHP() + 10 < other.getmaxHP()) {
+      other.setHP(other.getHP() + 10);
+    }
+    else {
+      other.setHP(other.getmaxHP());
+    }
+    return other.getName() + "was healed for 10 HP!";
+  }
+
+  public String support() {
+    setmaxHP(getmaxHP() + 5);
+    setHP(getHP() + 5);
+    if (getMP() - 10 > 0) {
+      setMP(getMP() - 10);
+    }
+    else {
+      return "Insufficient MP!";
+    }
+    return "Your total health has increased by 5 points!";
+  }
+
+  public String specialAttack(Adventurer other) {
+    if (other.getHP() - 30 > 0) {
+      other.applyDamage(30);
+    }
+    else {
+      other.setHP(0);
+    }
+    if (getMP() - 20 > 0) {
+      setMP(getMP() - 20);
+    }
+    else {
+      return "Insufficient MP!";
+    }
+    return "30 damage dealt with Fireball!";
+  }
+
+  public int getMP(){
+      return mp;
+  }
+
+  public int getmaxMP(){
+      return maxMP;
+  }
+
+  public void setMP(int mp){
+      this.mp = mp;
+  }
 }
